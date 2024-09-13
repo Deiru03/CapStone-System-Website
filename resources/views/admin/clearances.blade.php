0f659a92eccb3cb66f0b6dc45804f0df8556abed
Deiru <deiru03@gmail.com>
2024-09-13 18:23:49 +0800

    Clearance Fix but edit not

--- resources/views/admin/clearances.blade.php
+++ resources/views/admin/clearances.blade.php
@@ -50,7 +50,12 @@
                     <td class="py-3 px-4 text-center">{{ $users->clearance_status }}</td>
                     <td class="py-3 px-4">{{ $users->last_updated }}</td>
                     <td class="py-3 px-4">
-                        <button onclick="openModal({{ $users->id }})" class="text-blue-500 hover:text-blue-700">Edit</button>
+                        <button onclick="openModal({{ $users->id }})" class="text-blue-500 hover:text-blue-700 flex items-center">
+                            <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
+                            </svg>
+                            Edit
+                        </button>
                     </td>
                 </tr>
                 @endforeach
